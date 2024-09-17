7360e1b791f4442b1c5c998ac8d6ab715e4ebbad
Paul O'Connell <p_ollie@hotmail.com>
2024-09-17 16:20:58 -0700

    Update: pushed weather app and old profile to top -> updated weather app to use new API ect. in codePen

--- src/components/WebApps.js
+++ src/components/WebApps.js
@@ -12,6 +12,7 @@ const WebApps = function () {
     <div className="container-fluid">
       <MyWork />
 
+
       <h2 className=" my-3 textAlign">
         <span className="readEasy title text text-dark">Web Applications</span>
       </h2>
@@ -22,6 +23,52 @@ const WebApps = function () {
         </span>
       </h3>
 
+      <div className="row mx-1 justify-content-center">
+      <section className="col-12 col-sm-5 my-1 px-0 link">
+          <a
+            className="link d-block"
+            href="https://codepen.io/p_ollie/full/VMVeQw"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="text-align">
+              <span className="title text inlineBlock">
+                <strong>Wiki Search via API</strong>
+              </span>
+
+              <img
+                className="responsive"
+                src="https://res.cloudinary.com/pollie/image/upload/v1598909611/Wiki_search_avfb5n.png"
+                alt="wiki search"
+              />
+            </div>
+          </a>
+        </section>
+        <div className="col-sm-auto"></div>
+        
+        <section className="col-12 col-sm-5 my-1 px-0 link">
+          <a
+            className="link d-block"
+            title="Profile developed with pure HTML and CSS"
+            href="https://codepen.io/p_ollie/full/ayJXyZ"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className=" text-align">
+              <span className="title text inlineBlock">
+                <strong>Web Developer Profile (old) </strong>
+              </span>
+
+              <img
+                className="responsive  mt-md-3 "
+                src="https://res.cloudinary.com/pollie/image/upload/v1631908054/profile_v31u4b.png"
+                alt="Web Developer"
+              />
+            </div>
+          </a>
+        </section>
+      
+      </div>
       <div className="row mx-1 justify-content-center">
         <section className="col-12 col-sm-5 my-1 px-0 link">
           <a
@@ -170,50 +217,6 @@ const WebApps = function () {
         </section>
       </div>
 
-      <div className="row mx-1 justify-content-center">
-        <section className="col-12 col-sm-5 my-1 px-0 link">
-          <a
-            className="link d-block"
-            title="Profile developed with pure HTML and CSS"
-            href="https://codepen.io/p_ollie/full/ayJXyZ"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <div className=" text-align">
-              <span className="title text inlineBlock">
-                <strong>Web Developer Profile </strong>
-              </span>
-
-              <img
-                className="responsive  mt-md-3 "
-                src="https://res.cloudinary.com/pollie/image/upload/v1631908054/profile_v31u4b.png"
-                alt="Web Developer"
-              />
-            </div>
-          </a>
-        </section>
-        <div className="col-sm-auto"></div>
-        <section className="col-12 col-sm-5 my-1 px-0 link">
-          <a
-            className="link d-block"
-            href="https://codepen.io/p_ollie/full/VMVeQw"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <div className="text-align">
-              <span className="title text inlineBlock">
-                <strong>Wiki Search via API</strong>
-              </span>
-
-              <img
-                className="responsive"
-                src="https://res.cloudinary.com/pollie/image/upload/v1598909611/Wiki_search_avfb5n.png"
-                alt="wiki search"
-              />
-            </div>
-          </a>
-        </section>
-      </div>
 
       <div className="my-3">
         <MyWork />
