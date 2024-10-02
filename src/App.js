5b80296ee36543792281df4b6c411f8fd817ad4e
Paul O'Connell <p_ollie@hotmail.com>
2024-10-01 18:00:02 -0700

    Update: lazy loading components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -10,29 +10,38 @@ import './App.css';
 /* Import Components */
 import Header from './components/Header';
 import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import FullStack from './components/FullStack';
-import DataVisualization2 from './components/DataVisualization2';
-import WebApps from './components/WebApps';
+const About = lazy(() => import('./components/About'));
+const Contact = lazy(() => import('./components/Contact'));
+const FullStack = lazy(() => import('./components/FullStack'));
+const DataVisualization2 = lazy(() => import('./components/DataVisualization2'));
+const WebApps = lazy(() => import('./components/WebApps'));
+
+// import Header from './components/Header';
+// import Home from './components/Home';
+// import About from './components/About';
+// import Contact from './components/Contact';
+// import FullStack from './components/FullStack';
+// import DataVisualization2 from './components/DataVisualization2';
+// import WebApps from './components/WebApps';
 
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <div>
-          <Header />
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/fullStack" element={<FullStack />} />
-            <Route path="/dataVisualization2" element={<DataVisualization2 />} />
-            <Route path="/WebApps" element={<WebApps />} />
-          </Routes>
-        </div>
-      </Router>
+
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<Suspense fallback={<div>Loading...</div>}><About /></Suspense>} />
+          <Route path="/contact" element={<Suspense fallback={<div>Loading...</div>}><Contact /></Suspense>} />
+          <Route path="/fullStack" element={<Suspense fallback={<div>Loading...</div>}><FullStack /></Suspense>} />
+          <Route path="/dataVisualization2" element={<Suspense fallback={<div>Loading...</div>}><DataVisualization2 /></Suspense>} />
+          <Route path="/WebApps" element={<Suspense fallback={<div>Loading...</div>}><WebApps /></Suspense>} />
+        </Routes>
+    
+    
+      </Router >
     </div>
   );
 }
